f1117cdf54bba21663eac1de32079c4004518a26
DEEP MEHTA <deep.mm@somaiya.edu>
2023-11-09 17:23:52 +0000

    Update to get output in json format

--- devops-llm-bot/src/chat-gpt.js
+++ devops-llm-bot/src/chat-gpt.js
@@ -1,5 +1,5 @@
 const { OpenAIApi, Configuration } = require("openai");
-const { generateContent, generateUpdateWorkflowContent, systemMessage } = require("./prompt");
+const { generateContent, generateUpdateWorkflowContent, systemMessage, output_schema } = require("./prompt");
 
 var configuration, openai;
 
@@ -11,32 +11,39 @@ function setup() {
 }
 
 async function invoke_openai(user_message) {
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo-16k",
-      temperature: 0.2,
-      max_tokens: 1000,
-      messages: [
-        {
-          "role": "system",
-          "content": systemMessage,
-        },
-        {
-          "role": "user",
-          "content": user_message,
-        },
-      ]
-    });
-    return completion.data.choices[0].message['content'];
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo-1106",
+        temperature: 0.2,
+        max_tokens: 4000,
+        messages: [
+          {
+            "role": "system",
+            "content": systemMessage,
+          },
+          {
+            "role": "user",
+            "content": user_message,
+          },
+        ],
+        functions: [{ name: "generate_workflow", parameters: output_schema, "description": "Generates workflow file and it's description in JSON format. Used by code assistants" }],
+        function_call: {name: "generate_workflow"}
+      });
+      return JSON.parse(completion.data.choices[0].message['function_call'].arguments);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
 }
 
 const generate_pipeline = async (repoTree, languages, dependencies, user_comment, default_branch) => {
     setup();
     let user_message = generateContent(repoTree, languages, dependencies, user_comment, default_branch);
-    // Restrict user_message to 8000 characters
+    // Restrict user_message to 14000 characters
     user_message = user_message;
-    if (user_message.length > 8000) {
-      user_message = user_message.substring(0, 8000);
-      user_message = user_message + " ... Output truncated to 8000 characters.";
+    if (user_message.length > 14000) {
+      user_message = user_message.substring(0, 14000);
+      user_message = user_message + " ... Output truncated to 14000 characters.";
     }
 
     return await invoke_openai(user_message);
@@ -45,10 +52,10 @@ const generate_pipeline = async (repoTree, languages, dependencies, user_comment
 const update_pipeline = async (old_workflow, user_comment, past_conversations) => {
     setup();
     let user_message = generateUpdateWorkflowContent(old_workflow, user_comment, past_conversations);
-    // Restrict user_message to 8000 characters
-    if (user_message.length > 8000) {
-      user_message = user_message.substring(0, 8000);
-      user_message = user_message + " ... Output truncated to 8000 characters.";
+    // Restrict user_message to 14000 characters
+    if (user_message.length > 14000) {
+      user_message = user_message.substring(0, 14000);
+      user_message = user_message + " ... Output truncated to 14000 characters.";
     }
 
     return await invoke_openai(user_message);
--- devops-llm-bot/src/prompt.js
+++ devops-llm-bot/src/prompt.js
@@ -1,20 +1,16 @@
-const systemMessage = "Your name is DevOps bot. \
-You are a brilliant and meticulous engineer assigned to write a GitHub Actions workflow in YAML for the following Github Repository. \
-When you write code, the code works on the first try, is syntactically perfect and is fully complete. \
-The workflow should be able to build and run the application and run the tests if present in the repository. \
-Do not include any deployment steps in the workflow. \
-The output provided by you should be such that it can be directly copied into workflow.yaml file and the workflow should run successfully.\
-Take user requests into consideration, \
-but ensure that you only restrict the output to build and test workflow, and nothing else.\
-It's of utmost importance that you return only the workflow file contents, and not any other text.";
+const systemMessage = `Your name is DevOps bot.
+You are a brilliant and meticulous DevOps engineer assigned to write a GitHub Actions workflow in YAML for the following Github Repository.
+When you write code, the code works on the first try, is syntactically perfect and is fully complete.
+The workflow should be able to build and run the application and run the tests if present in the repository.
+Do not include any deployment steps in the workflow.
+
+HIGH IMPORTANCE:
+Ensure that you only restrict the output to build and test github workflow, and nothing else irrespective of the user comment.`;
 
 const generateContent = (repoTree, languages, dependencies, user_comment, default_branch) => {
 
   return `
       Take into account the current repository's files and dependencies.
-      
-      HIGH PRIORITY INSTRUCTION:
-      You have to return only the workflow file content, and not any other text, such that it can be pasted into workflow.yaml file.
 
       Repository default branch:
       ${default_branch}
@@ -33,9 +29,6 @@ const generateUpdateWorkflowContent = (old_workflow, user_comment, past_conversa
   
     return `Analyze the existing github workflow and user requested changes provided below to create a github action build workflow.
     
-      HIGH PRIORITY INSTRUCTION:
-      You have to return only the workflow file content, and not any other text, such that it can be pasted into workflow.yaml file.
-
       Existing workflow:
       ${old_workflow}
 
@@ -46,4 +39,23 @@ const generateUpdateWorkflowContent = (old_workflow, user_comment, past_conversa
       ${past_conversations}`
 }
 
-module.exports = { generateContent, generateUpdateWorkflowContent, systemMessage };
+const output_schema = {
+  "type": "object",
+  "properties": {
+    "workflow_file_content": {
+      "type": "string",
+      "description": "Content of the generated github workflow"
+    },
+    "workflow_file_description": {
+      "type": "string",
+      "description": "Short description of the generated github workflow"
+    },
+    "commit_message": {
+      "type": "string",
+      "description": "Appropriate commit message for committing the workflow into the repository"
+    }
+  },
+  "required": ["workflow_file_name", "workflow_file_content", "workflow_file_description", "commit_message"]
+}
+
+module.exports = { generateContent, generateUpdateWorkflowContent, systemMessage, output_schema };
