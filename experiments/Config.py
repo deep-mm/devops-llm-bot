2028edbcdc2fe2119b582bfd2235cc2b0cfd0217
DEEP MEHTA <deep.mm@somaiya.edu>
2023-12-02 11:57:00 -0500

    Update to gpt 4

--- experiments/Config.py
+++ experiments/Config.py
@@ -1,8 +1,8 @@
 class Options:
     def __init__(self):
         # model parameters
-        self.model = "gpt-3.5-turbo-1106"
-        #self.model = "gpt-4-1106-preview"
+        #self.model = "gpt-3.5-turbo-1106"
+        self.model = "gpt-4-1106-preview"
         self.max_tokens = 4000
         self.temperature = 0.2
         self.seed = 101
